52694bf88d123fd1589b3c0d4e13817a7b5233a2
alan chang <chang_a@qwasar.io>
2023-06-21 21:22:04 +0000

    time for a break

--- my_readline.c
+++ my_readline.c
@@ -37,22 +37,33 @@ void init_my_readline(){
 }
 
 char* my_readline(int fd) {
+
+    unsigned int count = 0;
     char* buffer = malloc((READLINE_READ_SIZE + 1) * sizeof(char));
     if (buffer == NULL) {
         perror("Memory allocation failed");
         return NULL;
     }
 
-    ssize_t read_result = read(fd, buffer, READLINE_READ_SIZE);
-    if (read_result == -1) {
-        perror("Read error");
-        free(buffer);
-        return NULL;
+    ssize_t read_result;
+    while((read_result = read(fd, buffer, READLINE_READ_SIZE)) == READLINE_READ_SIZE) {
+        if (read_result == -1) { // handle errors
+            perror("Read error");
+            free(buffer);
+            return NULL;
 
-    } else if (read_result == 0) {
-        // End of file reached
+        } else if (read_result == 0) {
+            // End of file reached
+            free(buffer);
+            return NULL;
+        }
+        count += read_result;
+        printf("%s", buffer);
         free(buffer);
-        return NULL;
+        char* buffer = malloc((READLINE_READ_SIZE + 1) * sizeof(char));
+
+
+
     }
 
     buffer[READLINE_READ_SIZE] = '\0';
@@ -61,20 +72,18 @@ char* my_readline(int fd) {
 
 
 int main(int ac, char **av) {
-  char *str = NULL;
-  int fd = open("test.txt", O_RDONLY);
-  str = my_readline(fd);
-  printf("%s\n", str);
-  free(str);
-  
+    char *str = NULL;
+    int fd = open("test.txt", O_RDONLY);
+    str = my_readline(fd);
+    printf("%s\n", str);
+    free(str);
+
 //   while ((str = my_readline(fd)) != NULL) {
-        
 //         printf("%s\n", str);
 //         free(str);
 //     }
 //     close(fd);
-        
-return 0;
+    return 0;
 } 
 
   //
