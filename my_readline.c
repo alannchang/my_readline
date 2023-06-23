7b44c1032c58be3f491fd4072b402bdddc82696b
alan chang <chang_a@qwasar.io>
2023-06-23 23:41:56 +0000

    my work is all cut out for me

--- my_readline.c
+++ my_readline.c
@@ -21,33 +21,32 @@ void init_my_readline(){
 char* my_readline(int fd) {
 
     unsigned int total_bytes = 0; // keep track of total size
-    char* main_warehouse = NULL; // stores 
-    char* buffer = malloc((READLINE_READ_SIZE + 1) * sizeof(char));
-    bool end_line = false;
-    if (buffer == NULL) {
+    char* rd_line_buffer = NULL;  // stores 'line' to be returned by my_readline
+    char* rd_buffer = malloc((READLINE_READ_SIZE + 1) * sizeof(char)); // buffer used for read()
+    bool end_line = false; // set to 'true' when '\n' encountered
+    if (rd_buffer == NULL) {
         perror("Memory allocation failed");
         return NULL;
     }
 
     ssize_t read_result; // number of bytes read
-    while((read_result = read(fd, buffer, READLINE_READ_SIZE)) > -1 && end_line == false) {
+    while ((read_result = read(fd, rd_buffer, READLINE_READ_SIZE)) > -1 && end_line == false) {
         if (read_result == -1) { // handle errors
             perror("Read error");
-            free(buffer);
+            free(rd_buffer);
             return NULL;
 
-        } else if (read_result == 0) {
-            // End of file reached
-            free(buffer);
+        } else if (read_result == 0) { // End of file reached
+            free(rd_buffer);
             return NULL;
         }
 
-        for (int i = 0; i < read_result; i++) {
-            if (buffer[i] == '\n') {
+        for (int i = 0; i < read_result; i++) { // check for '\n'
+            if (rd_buffer[i] == '\n') {
                 read_result = i;
                 end_line = true;
                 break;
-            } 
+            }
         }
         
         total_bytes += read_result;
@@ -56,28 +55,30 @@ char* my_readline(int fd) {
         char* temp = malloc((total_bytes + 1) * sizeof(char));
         if (temp == NULL) {
             perror("Memory allocation failed");
-            free(main_warehouse);
-            free(buffer);
+            free(rd_line_buffer);
+            free(rd_buffer);
             return NULL;
         }
 
         // Copy the existing data from storage to temp
-        if (main_warehouse != NULL) {
-            memcpy(temp, main_warehouse, total_bytes - read_result);
-            free(main_warehouse); // Free the old storage memory
+        if (rd_line_buffer != NULL)
+        {
+            memcpy(temp, rd_line_buffer, total_bytes - read_result);
+            free(rd_line_buffer);  // Free the old storage memory
         }
 
         // Copy the new data from buffer to temp
-        memcpy(temp + total_bytes - read_result, buffer, read_result);
+        memcpy(temp + total_bytes - read_result, rd_buffer, read_result);
 
         // Update storage to point to the new combined data in temp
-        main_warehouse = temp;
+        rd_line_buffer = temp;
         if (end_line == true) break;
+        // code memcpy to leftover here 
     }
 
-    main_warehouse[total_bytes] = '\0';
-    free(buffer);
-    return main_warehouse;
+    rd_line_buffer[total_bytes] = '\0';
+    free(rd_buffer);
+    return rd_line_buffer;
 }
 
 
