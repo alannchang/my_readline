98bc7ae2c9b87527aa6bfca6ec0051b8709eae0e
alan chang <chang_a@qwasar.io>
2023-06-23 23:31:19 +0000

    last line of file not showing up, need to code 'leftovers'

--- my_readline.c
+++ my_readline.c
@@ -7,21 +7,21 @@
 #include <string.h>
 
 // "Number of characters read will be set by the global variable READLINE_READ_SIZE"
-int READLINE_READ_SIZE = 100;
+int READLINE_READ_SIZE = 2;
 
 // store stuff here? ¯\_(ツ)_/¯
-char* storage = NULL;
+char* leftovers = NULL;
 
 // init (or reinitialize) global variable
 void init_my_readline(){
-    if (storage != NULL) free(storage);
-    storage = NULL;
+    // code leftovers to hold whatever comes after '\n'.  That's why things are cutting off.
+    // not sure why the last line is not printing though.
 }
 
 char* my_readline(int fd) {
 
     unsigned int total_bytes = 0; // keep track of total size
-    char* temp = NULL; // stores stuff
+    char* main_warehouse = NULL; // stores 
     char* buffer = malloc((READLINE_READ_SIZE + 1) * sizeof(char));
     bool end_line = false;
     if (buffer == NULL) {
@@ -53,31 +53,31 @@ char* my_readline(int fd) {
         total_bytes += read_result;
 
         // Allocate new memory for temp, which will store the new combined data
-        temp = malloc((total_bytes + 1) * sizeof(char));
+        char* temp = malloc((total_bytes + 1) * sizeof(char));
         if (temp == NULL) {
             perror("Memory allocation failed");
-            free(storage);
+            free(main_warehouse);
             free(buffer);
             return NULL;
         }
 
         // Copy the existing data from storage to temp
-        if (storage != NULL) {
-            memcpy(temp, storage, total_bytes - read_result);
-            free(storage); // Free the old storage memory
+        if (main_warehouse != NULL) {
+            memcpy(temp, main_warehouse, total_bytes - read_result);
+            free(main_warehouse); // Free the old storage memory
         }
 
         // Copy the new data from buffer to temp
         memcpy(temp + total_bytes - read_result, buffer, read_result);
 
         // Update storage to point to the new combined data in temp
-        storage = temp;
+        main_warehouse = temp;
         if (end_line == true) break;
     }
 
-    storage[total_bytes] = '\0';
+    main_warehouse[total_bytes] = '\0';
     free(buffer);
-    return storage;
+    return main_warehouse;
 }
 
 
@@ -89,7 +89,7 @@ int main(int ac, char **av)
   while ((str = my_readline(fd)) != NULL)
   {
       printf("%s\n", str);
-      init_my_readline();
+      free(str);
   }
   close(fd);
   //
